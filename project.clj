d241f155efe401f01f142f44f1780bd5dffe411f
Robert Kerr <rskerr@haystax.com>
2017-09-11 13:05:44 -0400

    cljs-rl episode 2

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject cljs-rl "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :description "A simple roguelike written in Clojurescript"
+  :url "http://localhost:10555"
+  :license {:name "The MIT License"
+            :url "https://opensource.org/licenses/MIT"}
 
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/clojurescript "1.9.671" :scope "provided"]
--- src/cljs/cljs_rl/db.cljs
+++ src/cljs/cljs_rl/db.cljs
@@ -1,4 +1,4 @@
 (ns cljs-rl.db)
 
 (def default-db
-  {:name "re-frame"})
+  {:name "ClojuresCrypt"})
